318aa1c3d1c94c986525fa46c44122dc614ac265
Igor Turovskiy <iturovskiy2497@gmail.com>
2018-05-03 13:53:41 +0300

    dev 0.08 очередные изменения лексера

--- i2_lexer.py
+++ i2_lexer.py
@@ -1,4 +1,5 @@
 import ply.lex as lex
+from re import sub
 
 MAX_SHORT = 2**8
 MAX_INT = 2**12
@@ -12,21 +13,27 @@ reserved = (
     'undefined',        # bool
     'right',            # direction
     'left'              # direction
+
+    #'int',              # var - точно ли?
+    #'short',            # var
+    #'bool',             # var
+    #'vector'            # var - что делать с vector of?
 )
 
 # словарь ключевых слов - токены
 keywords = {
-    'bool' : 'BOOL',
-    'int' : 'INT',
-    'short' : 'SHORT',
-    'vector' : 'VECTOR',
-
     'set' : 'SET',
     'add' : 'ADD',
     'sub' : 'SUB',
     'not' : 'NOT',
-    'or' : 'OR',
+    'or'  : 'OR',
     'and' : 'AND',
+
+    'int' : 'INT',
+    'short' : 'SHORT',
+    'bool' : 'BOOL',
+    'vector' : 'VECTOR',
+    'of' : 'OF',
 
     'function' : 'FUNCTION',
     'return' : 'RETURN',
@@ -46,7 +53,7 @@ keywords = {
 
 # кортеж токенов
 tokens = tuple(keywords.values()) + (
-    'ID', 'NUM', 'CMPARE', 'DIRECTION',
+    'ID', 'CINT', 'CSHORT', 'CMPARE', 'DIRECTION',
     'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET', 'LBRACE', 'RBRACE', 'PIPE',
     'ENDL', 'NEWLINE', 'COMMENT'
 )
@@ -62,11 +69,33 @@ t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
 t_LBRACE = r'\{'
 t_RBRACE = r'\}'
-#t_STRING = r'\".*?\"'
 t_ENDL = r';'
 
 
-def t_NUM(t):
+def t_CINT(t):
+    '(\-)?[1-9][0-9]*' # const int
+    t.value = int(t.value)
+    if abs(t.value) < MAX_INT:
+        return t
+    else:
+        # переполнение - ошибка
+        pass
+
+
+def t_CSHORT(t):
+    r'(\-)?[S][1-9][0-9]*'
+    t.value = sub('[S]', '', t.value)
+    t.value = int(t.value)
+    if abs(t.value) < MAX_SHORT:
+        return t
+    elif (MAX_SHORT <= abs(t.value) and abs(t.value) < MAX_INT):
+        t.type = 'CINT'
+        return t
+    elif (abs(t.value) >= MAX_INT):
+        # переполнение - ошибка
+        pass
+
+'''def t_NUM(t):
     r'\d+'
     t.value = int(t.value)
 
@@ -78,7 +107,7 @@ def t_NUM(t):
         return t
     else:
         # error - как обработать?
-        pass
+        pass'''
 
 
 def t_ID(t):
@@ -86,16 +115,18 @@ def t_ID(t):
     if t.value in keywords:
         t.type = keywords[t.value]
         return t
-    elif str(t.value).lower() in reserved:
-        if (str(t.value).lower() == reserved[2] or str(t.value).lower() == reserved[3]
-            or str(t.value).lower() == reserved[4]):
-            t.type = 'BOOL'
-        if (str(t.value).lower() == reserved[0] or str(t.value).lower() == reserved[1]):
+
+    elif t.value in reserved:
+        if (t.value == reserved[0] or t.value == reserved[1]):
             t.type = 'CMPARE'
-        if (str(t.value).lower() == reserved[5] or str(t.value).lower() == reserved[6]):
+        if (t.value == reserved[2] or t.value == reserved[3] or t.value == reserved[4]):
+            t.type = 'BOOL'
+        if (t.value == reserved[5] or t.value == reserved[6]):
             t.type = 'DIRECTION'
+        #if (t.value == reserved[7] or t.value == reserved[8] or t.value == reserved[9]):
+        #    t.type = 'VAR'
         return t
-    elif str(t.value).lower() in keywords:
+    elif (str(t.value).lower() in keywords or str(t.value).lower() in reserved):
         # error
         print('Illegal ID %s' %t.value)
 
@@ -123,9 +154,9 @@ def test(lexer, data):
 
 if __name__ == '__main__':
 	lexer = lex.lex() # (module=tokrules)
-	test(lexer, '''AD set 1300 add 4; // good\n
+	test(lexer, '''int A set 1300 add 4; // good
 		        move right;''')
 	print('')
-	test(lexer, "short A set 4;")
+	test(lexer, "short A set -S5;")
 	print('')
 	test(lexer, "short SHORT;")
